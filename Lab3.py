0e47810d3c6654a0b5b3f87fb36ca0b614da347f
AnastasiaRatko <anastasiaratnichenko@gmail.com>
2024-12-27 23:01:08 +0600

    fix

--- Lab3.py
+++ Lab3.py
@@ -1,5 +1,5 @@
 from ElevatorSystem import ElevatorSystem
 
-calls = [(5, 7), (3, 6), (2, 4), (1, 5), (7, 2), (4, 3), (6, 1), (2, 7)]
-elevator_system = ElevatorSystem(1, 1)
+calls = [(17, 12), (9,10)]
+elevator_system = ElevatorSystem(15, 8)
 elevator_system.process_calls(calls)
